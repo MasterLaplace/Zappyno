d14fb87836881b38ebec54727eaa4db9c0f35ec4
Mathis Le Bonniec <mathislbonniec@gmail.com>
2023-05-30 12:38:43 +0200

    [Feat]: Create Python3 architecture

---
+++ AI/src/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This program is an AI for the Zappy project.
+You can find more information with ./zappy_ai -h
+"""
+
+if __name__ == '__main__':
+    print('Hello World!')
