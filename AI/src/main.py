d1ae132812953c0ba54f9f9bef556ef20b9271a9
Mathieu Couronne <mathieu.couronne@epitech.eu>
2023-05-31 16:55:21 +0200

    [Feat@player]: creation of player at the beginning of the game

--- AI/src/main.py
+++ AI/src/main.py
@@ -4,15 +4,26 @@
 This program is an AI for the Zappy project.
 You can find more information with ./zappy_ai -h
 """
-from communication import Communication
+import sys
 
-communication: Communication = Communication('localhost', 3030)
+from communication import Communication
+from player import Player
+
+communication: Communication = Communication('localhost', 4242)
 
 if __name__ == '__main__':
     communication.connect()
+    communication.receive()
+    communication.send(sys.argv[2])
+    player_info = communication.receive()
+    if player_info == 'ko':
+        print('Invalid team name')
+        sys.exit(84)
+    Player(communication, sys.argv[2], player_info[1], player_info[0])
     while True:
         text: str = input('Enter text: ')
         if text == 'exit':
             break
         communication.send(text)
+        print(communication.receive())
     communication.disconnect()
