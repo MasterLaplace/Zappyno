77cd5fa2cae040c061aa024a885d4721162c3405
Mathieu Couronne <mathieu.couronne@epitech.eu>
2023-06-14 12:02:35 +0200

    [Refactor]: remove communication in main

--- AI/src/main.py
+++ AI/src/main.py
@@ -5,10 +5,7 @@ This program is an AI for the Zappy project.
 You can find more information with ./zappy_ai -h
 """
 from sys import argv
-from communication import Communication
 from client import Client
-
-communication: Communication = Communication('localhost', 4243)
 
 if __name__ == '__main__':
     client: Client = Client(argv[1], int(argv[2]), argv[3])
