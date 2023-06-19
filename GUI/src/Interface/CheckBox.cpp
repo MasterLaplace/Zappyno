b594b957295d45e6b02430a9f1dc57e662aedf2d
RaphaelLecoq <raphael.lecoq@epitech.eu>
2023-06-19 21:41:44 +0200

    [Fix@button] : fix assets in settings menu

---
+++ GUI/src/Interface/CheckBox.cpp
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2023
+** B-YEP-400-REN-4-1-zappy
+** File description:
+** CheckBox
+*/
+
+#include "../../includes/CheckBox.hpp"
+
+namespace Interface {
+    void Checkbox::updateState(const Math::Vector &mousePos, const bool &mousePressed) {
+        auto size = _sprite->getSize();
+        if (mousePos.x() >= _pos.x() && mousePos.x() <= _pos.x() + size.x() &&
+            mousePos.y() >= _pos.y() && mousePos.y() <= _pos.y() + size.y()) {
+            if (_state == IDLE)
+                _state = HOVER;
+            if (_state == CLICKED && !mousePressed) {
+                _state = RELEASED;
+                ischecked = !ischecked;
+            }
+            if (mousePressed)
+                _state = CLICKED;
+        } else {
+            _state = IDLE;
+        }
+    }
+
+    void Checkbox::animate_checkbox(const Math::Vector &mousePos, const bool &mousePressed) {
+        updateState(mousePos, mousePressed);
+        _sprite->animate_checkbox(ischecked, _state);
+    }
+} // namespace Interface
--- GUI/src/Interface/Panel.cpp
+++ GUI/src/Interface/Panel.cpp
@@ -11,12 +11,16 @@ namespace Interface {
     void Panel::updatePanel(const Math::Vector &mousePos, const bool &mousePressed) {
         for (auto &button : _buttons)
             button.animate(mousePos, mousePressed);
+        for (auto &checkbox : _checkboxs)
+            checkbox.animate_checkbox(mousePos, mousePressed);
         for (auto &bar : _bars)
             bar.updateState();
     }
     void Panel::updatePanel(const Math::Vector &mousePos, int key, const bool &mousePressed) {
         for (auto &button : _buttons)
             button.animate(mousePos, mousePressed);
+        for (auto &checkbox : _checkboxs)
+            checkbox.animate_checkbox(mousePos, mousePressed);
         for (auto &input : _inputs)
             input.updateState(mousePos, key, mousePressed);
         for (auto &bar : _bars)
@@ -27,6 +31,9 @@ namespace Interface {
         for (auto &button : _buttons)
             if (button.getState() == Interface::Button::RELEASED)
                 callback.push_back(button.getCallback());
+        for (auto &checkbox : _checkboxs)
+            if (checkbox.getState() == Interface::Checkbox::RELEASED)
+                callback.push_back(checkbox.getCallback());
         return callback;
     }
 }
