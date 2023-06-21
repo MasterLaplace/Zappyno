f8858058c7f60e84ac5c707c47a0706e9cd3097e
Mathis Le Bonniec <mathislbonniec@gmail.com>
2023-06-21 17:59:21 +0200

    [Feat@cli]: handle arguments from cli

--- AI/src/main.py
+++ AI/src/main.py
@@ -4,9 +4,23 @@
 This program is an AI for the Zappy project.
 You can find more information with ./zappy_ai -h
 """
-from sys import argv
 from client import Client
+from argparse import ArgumentParser, Namespace
+
+def get_arguments() -> ArgumentParser:
+    parser: ArgumentParser = ArgumentParser(add_help=False)
+
+    parser.add_argument('-p', type=int, help='is the port number (between 0 and 65535)', dest='port')
+    parser.add_argument('-n', type=str, help='is the name of the team', dest='name')
+    parser.add_argument('-h', type=str, help='is the name of the machine; localhost by default', default='localhost', dest='host')
+    return parser
 
 if __name__ == '__main__':
-    client: Client = Client(argv[1], int(argv[2]), argv[3])
+    arguments: ArgumentParser = get_arguments()
+
+    args: Namespace = arguments.parse_args()
+    if not args.port or not args.name or (args.port < 0 or args.port > 65535):
+        arguments.print_help()
+        exit(1)
+    client: Client = Client(args.host, args.port, args.name)
     client.loop()
