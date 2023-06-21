f0ce40da64b87e1dd112de7d7378477331e27eb7
Mathis Le Bonniec <mathislbonniec@gmail.com>
2023-06-21 18:04:09 +0200

    [Style@main]: arrange order and use sys.exit instead of exit

--- AI/src/main.py
+++ AI/src/main.py
@@ -4,15 +4,20 @@
 This program is an AI for the Zappy project.
 You can find more information with ./zappy_ai -h
 """
-from client import Client
 from argparse import ArgumentParser, Namespace
+from sys import exit as sys_exit
+from client import Client
 
 def get_arguments() -> ArgumentParser:
+    """
+    Instanciate the argument parser and add the arguments.
+    """
     parser: ArgumentParser = ArgumentParser(add_help=False)
 
     parser.add_argument('-p', type=int, help='is the port number (between 0 and 65535)', dest='port')
     parser.add_argument('-n', type=str, help='is the name of the team', dest='name')
-    parser.add_argument('-h', type=str, help='is the name of the machine; localhost by default', default='localhost', dest='host')
+    parser.add_argument('-h', type=str, help='is the name of the machine; localhost by default', default='localhost',
+        dest='host')
     return parser
 
 if __name__ == '__main__':
@@ -21,6 +26,6 @@ if __name__ == '__main__':
     args: Namespace = arguments.parse_args()
     if not args.port or not args.name or (args.port < 0 or args.port > 65535):
         arguments.print_help()
-        exit(1)
+        sys_exit(1)
     client: Client = Client(args.host, args.port, args.name)
     client.loop()
